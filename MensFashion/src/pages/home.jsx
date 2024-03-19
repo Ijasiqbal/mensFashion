1c02a297a9f1f8ab8be5f96371971c1ddfc2b92a
Ijas Iqbal <ijasiqbal4@gmail.com>
2024-03-19 10:42:36 +0530

    project started

--- MensFashion/src/pages/home.jsx
+++ MensFashion/src/pages/home.jsx
@@ -1,10 +1,11 @@
 import Navbar from "../components/Navbar";
+import PhotoContent from "../components/PhotoContent";
 
 export default function home() {
   return (
     <div>
         <Navbar/>
-              
+
     </div>
   )
 }
